5b0a7fed9afd928316b1f6bf147979e1856357d6
Ndhrbt1 <indahroad04@gmail.com>
2025-07-03 00:55:24 +0700

    build:copy template & create page

---
+++ lib/app/inits.dart
@@ -0,0 +1,10 @@
+part of '_index.dart';
+
+Future<void> inits() async {
+  logx.wtf('inits start');
+
+  Serv.sample.init();
+  Serv.product.init();
+
+  logx.wtf('inits success');
+}
---
+++ lib/app/navigation/a.inject.dart
@@ -0,0 +1,3 @@
+part of '_index.dart';
+
+final nav = NavigationBuilder.create(initialLocation: Routes.productList, routes: RouteMap.data);
