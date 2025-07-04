a2cb8df4bd27f58199ff9a61f7407776e6c752ec
Ndhrbt1 <indahroad04@gmail.com>
2025-07-05 06:36:31 +0700

    feat: CRUD (Test Connection) with update, set , setOption

--- lib/app/_index.dart
+++ lib/app/_index.dart
@@ -1,3 +1,5 @@
+import 'package:copy_template/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:states_rebuilder/states_rebuilder.dart';
 
