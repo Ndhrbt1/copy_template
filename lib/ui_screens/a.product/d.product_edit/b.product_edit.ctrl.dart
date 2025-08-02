d7862464a38b89a0c8ac47c7a7a936f1645452bb
Ndhrbt1 <indahroad04@gmail.com>
2025-08-02 12:10:43 +0700

    feat: update image

--- lib/ui_screens/a.product/d.product_edit/b.product_edit.ctrl.dart
+++ lib/ui_screens/a.product/d.product_edit/b.product_edit.ctrl.dart
@@ -8,13 +8,15 @@ class ProductEditCtrl {
   updateRandom() => Serv.sample.updateRandom();
 
   Future<void> updateDoc() async {
+    final id = _dt.rxProductDetail.st!.id;
     final productEdit = Product(
-      id: _dt.rxProductDetail.st!.id,
+      id: id,
       name: _dt.rxName.st.value,
       price: int.parse(_dt.rxPrice.st.value),
       qty: int.parse(_dt.rxQty.st.value),
       createdAt: _dt.rxProductDetail.st!.createdAt,
       updatedAt: DateTime.now().toString(),
+      imageUrl: await _sv.uploadImage(id),
     );
 
     _sv.updateDoc(productEdit);
@@ -22,4 +24,10 @@ class ProductEditCtrl {
   }
 
   submit() async => _dt.rxForm.submit();
+
+  Future<XFile?> pickImage() async {
+    _sv.pickImage();
+
+    return _dt.rxPickedImage.st;
+  }
 }
